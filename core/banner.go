239c6c2ef3614accf3a67972a69226d9d1cec1ac
zhangwei-g1t <118270714+zhangwei-g1t@users.noreply.github.com>
2022-11-15 16:23:20 +0100

    Update banner.go

--- core/banner.go
+++ core/banner.go
@@ -8,55 +8,20 @@ import (
 )
 
 const (
-	VERSION = "2.4.2"
+	VERSION = "0.0.1"
 )
 
 func putAsciiArt(s string) {
-	for _, c := range s {
-		d := string(c)
-		switch string(c) {
-		case " ":
-			color.Set(color.BgRed)
-			d = " "
-		case "@":
-			color.Set(color.BgBlack)
-			d = " "
-		case "#":
-			color.Set(color.BgHiRed)
-			d = " "
-		case "W":
-			color.Set(color.BgWhite)
-			d = " "
-		case "_":
-			color.Unset()
-			d = " "
-		case "\n":
-			color.Unset()
-		}
-		fmt.Print(d)
-	}
-	color.Unset()
+
 }
 
 func printLogo(s string) {
-	for _, c := range s {
-		d := string(c)
-		switch string(c) {
-		case "_":
-			color.Set(color.FgWhite)
-		case "\n":
-			color.Unset()
-		default:
-			color.Set(color.FgHiBlack)
-		}
-		fmt.Print(d)
-	}
-	color.Unset()
+
 }
 
 func printUpdateName() {
 	nameClr := color.New(color.FgHiRed)
-	txt := nameClr.Sprintf("                 - --  Gone Phishing  -- -")
+	txt := nameClr.Sprintf("                 - --  Go Phishing  -- -")
 	fmt.Fprintf(color.Output, "%s", txt)
 }
 
@@ -65,7 +30,7 @@ func printOneliner1() {
 	versionClr := color.New(color.FgGreen)
 	textClr := color.New(color.FgHiBlack)
 	spc := strings.Repeat(" ", 10-len(VERSION))
-	txt := textClr.Sprintf("      by Kuba Gretzky (") + handleClr.Sprintf("@mrgretzky") + textClr.Sprintf(")") + spc + textClr.Sprintf("version ") + versionClr.Sprintf("%s", VERSION)
+	txt := textClr.Sprintf("      by Zhang Wei Li (") + handleClr.Sprintf("@zhangwei-g1t") + textClr.Sprintf(")") + spc + textClr.Sprintf("version ") + versionClr.Sprintf("%s", VERSION)
 	fmt.Fprintf(color.Output, "%s", txt)
 }
 
@@ -73,7 +38,8 @@ func printOneliner2() {
 	textClr := color.New(color.FgHiBlack)
 	red := color.New(color.FgRed)
 	white := color.New(color.FgWhite)
-	txt := textClr.Sprintf("                   no ") + red.Sprintf("nginx") + white.Sprintf(" - ") + textClr.Sprintf("pure ") + red.Sprintf("evil")
+	txt := textClr.Sprintf("                   no ") + red.Sprintf("nginx") + white.Sprintf(" - ") + textClr.Sprintf("pure ") + red.Sprintf(" mod-2xniglive
+")
 	fmt.Fprintf(color.Output, "%s", txt)
 }
 

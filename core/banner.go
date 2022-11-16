0efe2aaaba5dad2bbdf942dd18035a9bea9b38ca
zhangwei-g1t <118270714+zhangwei-g1t@users.noreply.github.com>
2022-11-16 10:29:26 +0100

    Update banner.go

--- core/banner.go
+++ core/banner.go
@@ -12,11 +12,46 @@ const (
 )
 
 func putAsciiArt(s string) {
-
+	for _, c := range s {
+		d := string(c)
+		switch string(c) {
+		case " ":
+			color.Set(color.BgRed)
+			d = " "
+		case "@":
+			color.Set(color.BgBlack)
+			d = " "
+		case "#":
+			color.Set(color.BgHiRed)
+			d = " "
+		case "W":
+			color.Set(color.BgWhite)
+			d = " "
+		case "_":
+			color.Unset()
+			d = " "
+		case "\n":
+			color.Unset()
+		}
+		fmt.Print(d)
+	}
+	color.Unset()
 }
 
 func printLogo(s string) {
-
+	for _, c := range s {
+		d := string(c)
+		switch string(c) {
+		case "_":
+			color.Set(color.FgWhite)
+		case "\n":
+			color.Unset()
+		default:
+			color.Set(color.FgHiBlack)
+		}
+		fmt.Print(d)
+	}
+	color.Unset()
 }
 
 func printUpdateName() {
@@ -30,7 +65,7 @@ func printOneliner1() {
 	versionClr := color.New(color.FgGreen)
 	textClr := color.New(color.FgHiBlack)
 	spc := strings.Repeat(" ", 10-len(VERSION))
-	txt := textClr.Sprintf("      by Zhang Wei Li (") + handleClr.Sprintf("@zhangwei-g1t") + textClr.Sprintf(")") + spc + textClr.Sprintf("version ") + versionClr.Sprintf("%s", VERSION)
+	txt := textClr.Sprintf("      Mod by Zhang Wei Li (") + handleClr.Sprintf("@zhangwei-g1t") + textClr.Sprintf(")") + spc + textClr.Sprintf("version ") + versionClr.Sprintf("%s", VERSION)
 	fmt.Fprintf(color.Output, "%s", txt)
 }
 
@@ -45,12 +80,36 @@ func printOneliner2() {
 
 func Banner() {
 	fmt.Println()
+
+	putAsciiArt("__                                     __\n")
+	putAsciiArt("_   @@     @@@@@@@@@@@@@@@@@@@     @@   _")
+	printLogo(`    ___________      __ __           __               `)
 	fmt.Println()
+	putAsciiArt("  @@@@    @@@@@@@@@@@@@@@@@@@@@    @@@@  ")
+	printLogo(`    \_   _____/__  _|__|  |    ____ |__| ____ ___  ___`)
+	fmt.Println()
+	putAsciiArt("  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  ")
+	printLogo(`     |    __)_\  \/ /  |  |   / __ \|  |/    \\  \/  /`)
+	fmt.Println()
+	putAsciiArt("    @@@@@@@@@@###@@@@@@@###@@@@@@@@@@    ")
+	printLogo(`     |        \\   /|  |  |__/ /_/  >  |   |  \>    < `)
+	fmt.Println()
+	putAsciiArt("      @@@@@@@#####@@@@@#####@@@@@@@      ")
+	printLogo(`    /_______  / \_/ |__|____/\___  /|__|___|  /__/\_ \`)
+	fmt.Println()
+	putAsciiArt("       @@@@@@@###@@@@@@@###@@@@@@@       ")
+	printLogo(`            \/              /_____/         \/      \/`)
+	fmt.Println()
+	putAsciiArt("      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@      \n")
+	putAsciiArt("     @@@@@WW@@@WW@@WWW@@WW@@@WW@@@@@     ")
 	printUpdateName()
 	fmt.Println()
-	printOneliner2()
-	fmt.Println()
+	putAsciiArt("    @@@@@@WW@@@WW@@WWW@@WW@@@WW@@@@@@    \n")
+	//printOneliner2()
+	//fmt.Println()
+	putAsciiArt("_   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   _")
 	printOneliner1()
 	fmt.Println()
+	putAsciiArt("__                                     __\n")
 	fmt.Println()
 }
